de6972c561fa00c85439e976f4e0271e2d51e5ae
Neil Macneale V <neil.macneale.v@gmail.com>
2025-03-22 20:56:11 -0700

    Add no_main and no_std

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-  println!("Hello, world!");
-}
+#![cfg_attr(not(test), no_main)]
+#![no_std]
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! { unimplemented!() }
