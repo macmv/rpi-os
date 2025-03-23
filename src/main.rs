4477ed95d3771756714f48da51f2bde0b82acb5f
Neil Macneale V <neil.macneale.v@gmail.com>
2025-03-22 22:17:19 -0700

    Implement some logging on panic

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,39 @@ fn _start_rust() -> ! {
 }
 
 #[cfg(not(test))]
+fn panic_prevent_reenter() {
+  static mut PANIC_IN_PROGRESS: bool = false;
+
+  unsafe {
+    if !PANIC_IN_PROGRESS {
+      PANIC_IN_PROGRESS = true;
+
+      return;
+    }
+  }
+
+  wait_forever()
+}
+
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
+  // Protect against panic infinite loops if any of the following code panics
+  // itself.
+  panic_prevent_reenter();
+
+  let (location, line, column) = match info.location() {
+    Some(loc) => (loc.file(), loc.line(), loc.column()),
+    _ => ("???", 0, 0),
+  };
+
+  println!("=== kernel panic ===\nat {}:{}:{}: {}", location, line, column, info.message());
+
+  wait_forever()
+}
+
+#[cfg(not(test))]
+fn wait_forever() -> ! {
   loop {
     arch::wfe();
   }
