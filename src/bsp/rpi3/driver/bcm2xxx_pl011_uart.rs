f6a9d682bfab4a9ff9882328f529775e652a4f26
Neil Macneale V <neil.macneale.v@gmail.com>
2025-03-23 00:08:22 -0700

    Add a macro for register field offsets

--- src/bsp/rpi3/driver/bcm2xxx_pl011_uart.rs
+++ src/bsp/rpi3/driver/bcm2xxx_pl011_uart.rs
@@ -1,6 +1,9 @@
 use core::fmt;
 
-use crate::register::Reg;
+use crate::{
+  reg_struct,
+  register::{RegRO, RegRW},
+};
 
 #[repr(C)]
 pub struct PL011Uart {
@@ -15,29 +18,25 @@ pub unsafe fn init() {
   }
 }
 
-#[repr(C)]
+reg_struct! {
 struct PL011UartRegister {
-  dr:         Reg<u32>,
-  rsrecr:     Reg<u32>,
-  _reserved0: [u32; 4],
-  fr:         Reg<u32>,
-  _reserved1: [u32; 1],
-  ilpr:       Reg<u32>,
-  ibrd:       Reg<u32>,
-  fbrd:       Reg<u32>,
-  lcrh:       Reg<u32>,
-  cr:         Reg<u32>,
-  ifls:       Reg<u32>,
-  imsc:       Reg<u32>,
-  ris:        Reg<u32>,
-  mis:        Reg<u32>,
-  icr:        Reg<u32>,
-  dmacr:      Reg<u32>,
-  _reserved2: [u32; 13],
-  itcr:       Reg<u32>,
-  itip:       Reg<u32>,
-  itop:       Reg<u32>,
-  tdr:        Reg<u32>,
+  0x00 -> dr:         RegRW<u32>,
+  0x04 -> rsrecr:     RegRW<u32>,
+  0x08 -> _reserved0: [u32; 4],
+  0x18 -> fr:         RegRO<u32>,
+  0x1c -> _reserved1: [u32; 1],
+  0x20 -> ilpr:       RegRW<u32>,
+  0x24 -> ibrd:       RegRW<u32>,
+  0x28 -> fbrd:       RegRW<u32>,
+  0x2c -> lcrh:       RegRW<u32>,
+  0x30 -> cr:         RegRW<u32>,
+  0x34 -> ifls:       RegRW<u32>,
+  0x38 -> imsc:       RegRW<u32>,
+  0x3c -> ris:        RegRW<u32>,
+  0x40 -> mis:        RegRW<u32>,
+  0x44 -> icr:        RegRW<u32>,
+  0x48 -> dmacr:      RegRW<u32>,
+}
 }
 
 impl fmt::Write for PL011Uart {
@@ -62,32 +61,4 @@ impl PL011Uart {
 
   /// SAFETY: Must only be called once.
   unsafe fn init(&self) { self.reg().dr.set(0x80); }
-}
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-  use core::mem::offset_of;
-
-  #[test]
-  fn register_offsets() {
-    assert_eq!(offset_of!(PL011UartRegister, dr), 0x00);
-    assert_eq!(offset_of!(PL011UartRegister, rsrecr), 0x04);
-    assert_eq!(offset_of!(PL011UartRegister, fr), 0x18);
-    assert_eq!(offset_of!(PL011UartRegister, ilpr), 0x20);
-    assert_eq!(offset_of!(PL011UartRegister, ibrd), 0x24);
-    assert_eq!(offset_of!(PL011UartRegister, fbrd), 0x28);
-    assert_eq!(offset_of!(PL011UartRegister, lcrh), 0x2c);
-    assert_eq!(offset_of!(PL011UartRegister, cr), 0x30);
-    assert_eq!(offset_of!(PL011UartRegister, ifls), 0x34);
-    assert_eq!(offset_of!(PL011UartRegister, imsc), 0x38);
-    assert_eq!(offset_of!(PL011UartRegister, ris), 0x3c);
-    assert_eq!(offset_of!(PL011UartRegister, mis), 0x40);
-    assert_eq!(offset_of!(PL011UartRegister, icr), 0x44);
-    assert_eq!(offset_of!(PL011UartRegister, dmacr), 0x48);
-    assert_eq!(offset_of!(PL011UartRegister, itcr), 0x80);
-    assert_eq!(offset_of!(PL011UartRegister, itip), 0x84);
-    assert_eq!(offset_of!(PL011UartRegister, itop), 0x88);
-    assert_eq!(offset_of!(PL011UartRegister, tdr), 0x8c);
-  }
 }
--- src/register.rs
+++ src/register.rs
@@ -1,11 +1,49 @@
 use core::cell::UnsafeCell;
 
-pub struct Reg<T> {
+pub struct RegRW<T> {
   value: UnsafeCell<T>,
 }
+pub struct RegRO<T> {
+  value: T,
+}
 
-impl<T: Clone + Copy> Reg<T> {
+impl<T: Clone + Copy> RegRO<T> {
+  pub fn get(&self) -> T { self.value }
+}
+
+impl<T: Clone + Copy> RegRW<T> {
   pub fn get(&self) -> T { unsafe { core::ptr::read_volatile(self.value.get()) } }
 
   pub fn set(&self, value: T) { unsafe { core::ptr::write_volatile(self.value.get(), value) } }
+}
+
+#[macro_export]
+macro_rules! reg_struct {
+  (
+    struct $name:ident {
+      $(
+        $offset:literal -> $field:ident: $reg:ty,
+      )*
+    }
+  ) => {
+    #[repr(C)]
+    pub struct $name {
+      $(
+        pub $field: $reg,
+      )*
+    }
+
+    #[cfg(test)]
+    mod tests {
+      use super::*;
+      use core::mem::offset_of;
+
+      #[test]
+      fn register_offsets() {
+        $(
+          assert_eq!(offset_of!($name, $field), $offset);
+        )*
+      }
+    }
+  };
 }
