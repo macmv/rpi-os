6f5b408035bc9582408f920707271edd4554071f
Neil Macneale V <neil.macneale.v@gmail.com>
2025-03-24 01:33:37 -0700

    Move wfe to aarch64 module

--- src/arch/aarch64/mod.rs
+++ src/arch/aarch64/mod.rs
@@ -5,3 +5,6 @@ global_asm!(
   include_str!("boot.s"),
   CONST_CORE_ID_MASK = const 0b11
 );
+
+#[inline(always)]
+pub fn wfe() { unsafe { core::arch::asm!("wfe", options(nomem, nostack)) } }
--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -4,13 +4,10 @@ mod aarch64;
 #[cfg(target_arch = "aarch64")]
 pub use aarch64::*;
 
-#[inline(always)]
-pub fn wfe() {
-  #[cfg(target_arch = "aarch64")]
-  unsafe {
-    core::arch::asm!("wfe", options(nomem, nostack))
-  }
-
-  #[cfg(not(target_arch = "aarch64"))]
-  unimplemented!()
+#[cfg(not(target_arch = "aarch64"))]
+mod tmp {
+  pub fn wfe() { unimplemented!() }
 }
+
+#[cfg(not(target_arch = "aarch64"))]
+pub use tmp::*;
